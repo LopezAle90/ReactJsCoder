8b0d96cbda9da81263493576730e83fc8461efe9
Claudio Alejandro Lopez <alejandrolopezz21@gmail.com>
2024-12-30 22:23:23 -0300

    PreEntrega02-Lopez

--- src/components/CartWidget.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-
-export const CartWidget = () => {
-    return <div>
-        <i className="bi bi-cart-fill"> <span>1</span></i>
-    </div>
-}
-
--- src/components/ItemListContainer.js
+++
@@ -1,5 +0,0 @@
-export const ItemListContainer = ({greeting}) => {
-    return <div className="bienvenida">
-        <h2>{greeting}</h2>
-    </div>
-}
